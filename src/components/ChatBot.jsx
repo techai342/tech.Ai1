d1fa5fbb60a2d64d446aab82c04d8cbf5f149908
techai342 <tiktokusr4400@gmail.com>
2025-11-05 07:03:43 +0500

    Update ChatBot.jsx

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,4 +1,4 @@
-.import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { businessInfo, products, faqs } from "../data/FAQContent";
 
 export default function ChatBot({
